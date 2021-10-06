1149a719af3421a9b4846937427a14c6e1d1ab66
Davide Corsi <davide.corsi@par-tec.it>
2021-10-06 18:05:16 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 /*
  * Create un applicazione che chieda in input una somma di denaro, una percentuale di interesse ed
  * il numero di anni per il quale la somma di denaro verrà mantenuta dalla banca.
- * Per ogni anno la somma di denaro crescerà del 20% sul totale.
+ * Per ogni anno la somma di denaro crescerà della percentuale di interesse indicata.
  * Calcolare la somma finale. Utilizzare sottoprogrammi per la lettura dei valori, il calcolo della
  * somma finale e la stampa.
  */
